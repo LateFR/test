691d9b350b8c7fecc0ff8a67676b17c243f7bca9
LateFR <rigo.lucas@icloud.com>
2025-01-21 13:47:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 
-function toggle_opacity(li_element){
-    if (li_element){
-    li_element.classList.toggle("visible")
-    }
-  }
+function create_element(){
+    new_li=document.createElement("li")
+    new_li.innerText="hi"
+    new_li.setAttribute("class","the_li")
+    document.querySelector("ul").appendChild(new_li)
+    retrun new_li
+}
 
 const recroll=(entries, observer)=>{
-  entries.forEach(element=>{
-    toggle_opacity(element)
-  })
+    if (entries.isIntersecting){
+        new_li=create_element()
+        infiniteScrollObserver(new_li)
+    }
 }
 
 
-const infinitScrollObserver= new IntersectionObserver(recroll,{
-  rootMargin:"5px",
-  threshold: 0.0
+const infiniteScrollObserver= new IntersectionObserver(recroll,{
+    rootMargin:"5px",
+    threshold: 0.0
 })
 
 for(i=0;i<100;i++){
-  new_li=document.createElement("li")
-  new_li.innerText="hi"
-  new_li.setAttribute("class","the_li")
-  document.querySelector("ul").appendChild(new_li)
+    new_li=create_element()
+    infiniteScrollObserver.observe(new_li)
 }
 
 document.querySelectorAll("the_li").forEach(new_li=>{
-  infinitScrollObserver.observe(new_li)
-}
-)
+      infinitScrollObserver.observe(new_li)
+})
