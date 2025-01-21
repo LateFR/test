4aa399c15c89de77b5c276ad0cb8e2d1cb80f8e4
LateFR <rigo.lucas@icloud.com>
2025-01-21 10:52:03 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+for(i=0;i<100;i++){
+  new_li=document.createElement("li")
+  new_li.innerText="hi"
+  new_li.setAttribute("class","the_li")
+  document.querySelector("ul").appendChild(new_li)
+}
+
+function toogle_opacity(){
+  li_elements=document.querySelectorAll(".the_li")
+  li_elements.forEach((element)=>{
+    element
+  })
+}
